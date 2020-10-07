382c481485d7fd37b5e4d75fc9efced12b3fcf9c
Sofiya Kuskova (VS) <kuskova-sofya@mail.ru>
2020-10-07 09:56:43 +0300

    Added 4 lab

---
+++ lab4/lab4/List.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace lab4
+{
+    public class List
+    {
+        public class Owner
+        {
+            public int ID { get; private set; }
+            public string Name { get; private set; }
+            public string Company { get; private set; }
+            public Owner(string name, string company)
+            {
+                ID = DateTime.Now.GetHashCode();
+                Name = name;
+                Company = company;
+            }
+        }
+
+        int[] elements;
+        public int Length { get; private set; }
+
+        public Owner owner;
+        public DateTime date;
+
+        public int this[int i]
+        {
+            get { return elements[i]; }
+            set { elements[i] = value; }
+        }
+        private List()
+        {
+            owner = new Owner("Sofiya", "KHCorp");
+            date = new DateTime();
+        }
+
+        public List(int[] elements) : this()
+        {
+            this.elements = elements;
+            Length = elements.Length;
+
+            string str = String.Empty;
+        }
+        public List(int size) : this()
+        {
+            elements = new int[size];
+            Length = size;
+            for (int i = 0; i < Length; i++)
+            {
+                elements[i] = Program.rand.Next(-10, 10);
+            }
+        }
+
+        public static List operator>>(List left, int right)
+        {
+            right--;
+            int[] buffer = new int[left.Length - 1];
+            for (int i = 0; i < right; i++)
+            {
+                buffer[i] = left.elements[i];
+            }
+            for (int i = right; i < buffer.Length; i++)
+            {
+                buffer[i] = left.elements[i + 1];
+            }
+            return new List(buffer);
+        }
+        
+        public static List operator+(List left, int right)
+        {
+            int[] buffer = new int[left.Length + 1];
+            for (int i = 0; i < right; i++)
+            {
+                buffer[i] = left.elements[i];
+            }
+            buffer[--right] = Program.rand.Next(-10, 10);
+            for (int i = right + 1; i < buffer.Length; i++)
+            {
+                buffer[i] = left.elements[i - 1];
+            }
+            return new List(buffer);
+        }
+
+        public static bool operator !=(List left, List right)
+        {
+            if (left.Length != right.Length) return true;
+            for (int i = 0; i < left.Length; i++)
+            {
+                if (left.elements[i] != right.elements[i]) return true;
+            }
+            return false;
+        }
+
+        public static bool operator ==(List left, List right)
+        {
+            return !(left != right);
+        }
+
+        public override string ToString()
+        {
+            string result = String.Empty;
+            for (int i = 0; i < Length; i++)
+            {
+                result += $"\t{elements[i]}";
+            }
+            return result;
+        }
+    }
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+        public static Random rand;
+        static void Main(string[] args)
+        {
+            rand = new Random(DateTime.Now.Millisecond);
+
+            List list = new List(5);
+            Console.WriteLine(list);
+            Console.WriteLine(list >> 2);
+            Console.WriteLine(list);
+            Console.WriteLine(list + 3);
+            Console.WriteLine(list);
+            Console.WriteLine(list != list);
+            Console.WriteLine(list == list);
+
+            string words = Console.ReadLine();
+            Console.WriteLine("Longest word - " + words.GetLongestWord());
+            Console.WriteLine(list.DeleteLastElement());
+            list.MaxMinDif();
+            Console.ReadKey();
+        }
+    }
+    public static class Extensions
+    {
+        public static int Sum(this List source)
+        {
+            int sum = 0;
+            for (int i = 0; i < source.Length; i++)
+            {
+                sum += source[i];
+            }
+            return sum;
+        }
+        public static int MaxMinDif(this List source)
+        {
+            int mx = int.MinValue, mn = int.MaxValue;
+            for (int i = 0; i < source.Length; i++)
+            {
+                mx = Math.Max(mx, source[i]);
+                mn = Math.Min(mn, source[i]);
+            }
+            return mx - mn;
+        }
+        public static int Count(this List source)
+        {
+            return source.Length;
+        }
+        public static string GetLongestWord(this string source)
+        {
+            string[] words = source.Split(' ');
+            return words.Max();
+        }
+
+        public static List DeleteLastElement(this List source)
+        {
+            return source >>= source.Length;
+        }
+    }
+}
