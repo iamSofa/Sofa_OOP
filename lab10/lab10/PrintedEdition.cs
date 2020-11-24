91e0ac5e7d00fa09f737e81250c1220a97717a15
iamSofa <kuskova-sofya@mail.ru>
2020-11-24 19:54:11 +0300

    Добавлены 3, 4 задания

---
+++ lab10/lab10/PrintedEdition.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class PrintedEdition
+    {
+        public string title;
+        public short publishingYear;
+        public int pageAmount;
+
+        public PrintedEdition ()
+        {
+            Title = "No name";
+            PublishingYear = 2018;
+            PageAmount = 23;
+        }
+        public int PageAmount
+        {
+            get { return pageAmount; }
+            set
+            {
+                if (value < 1) return;
+                else pageAmount = value;
+            }
+        }
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public short PublishingYear
+        {
+            get { return publishingYear; }
+            set
+            {
+                publishingYear = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Title}    {PublishingYear}    {PageAmount}";
+        }
+    }
+}
--- lab10/lab10/Program.cs
+++ lab10/lab10/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace lab10
 {
@@ -12,6 +14,7 @@ namespace lab10
         static void Main(string[] args)
         {
             //1
+            Console.WriteLine("Задание 1\n");
             ArrayList arrList = new ArrayList();
             Random rand = new Random();
             for (int i = 0; i < 7; i++)
@@ -31,7 +34,7 @@ namespace lab10
             {
                 Console.Write(i + "   ");
             }
-            Console.WriteLine("\nКоличество элементов : " + arrList.Count);
+            Console.WriteLine("\nКоличество элементов: " + arrList.Count);
             Console.Write("Введите элемент, который надо найти: ");
             if (arrList.Contains(Console.ReadLine()))
             {
@@ -43,6 +46,7 @@ namespace lab10
             }
 
             //2
+            Console.WriteLine("\nЗадание 2\n");
             LinkedList<char> LinkList = new LinkedList<char>();
             LinkList.AddLast('a');
             LinkList.AddFirst('c');
@@ -54,6 +58,7 @@ namespace lab10
                 Console.Write(i + " ");
             }
             Console.WriteLine();
+            Console.WriteLine("\nВведите количство элементов для удаления: ");
             int n = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
@@ -83,7 +88,7 @@ namespace lab10
             {
                 Console.Write(i + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine("\nВведите символ: ");
             if (hash.Contains(Convert.ToChar(Console.ReadLine())))
             {
                 Console.WriteLine("Этот элемент присутствует в коллекции\n");
@@ -91,6 +96,108 @@ namespace lab10
             else
             {
                 Console.WriteLine("В коллекции нет такого элемента\n");
+            }
+            Console.WriteLine();
+
+            //3
+            Console.WriteLine("\nЗадание 3\n");
+            LinkedList<PrintedEdition> book = new LinkedList<PrintedEdition>();
+            LinkedListNode<PrintedEdition> book1 = book.AddLast(new PrintedEdition() { Title = "Book2", PublishingYear = 1879});
+            book.AddLast(new PrintedEdition() { Title = "Book3", PublishingYear = 2009 });
+            book.AddFirst(new PrintedEdition() { Title = "Book1", PublishingYear = 2019 });
+            book.AddLast(new PrintedEdition() { Title = "Book5", PublishingYear = 1908, PageAmount = 123 });
+            foreach (PrintedEdition i in book)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("\nВведите количство элементов для удаления: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                book.RemoveLast();
+            }
+            foreach (PrintedEdition i in book)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            PrintedEdition book2 = new PrintedEdition
+            {
+                PageAmount = 123
+            };
+            LinkedListNode<PrintedEdition> node1 = book.First;
+            book.AddAfter(node1, book2);
+            foreach (PrintedEdition i in book)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            HashSet<PrintedEdition> hash2 = new HashSet<PrintedEdition>();
+            node1 = book.First;
+            while (node1 != null)
+            {
+                hash2.Add(node1.Value);
+                node1 = node1.Next;
+            }
+            Console.WriteLine("Вывод элементов из коллекции HashSet");
+            foreach (PrintedEdition i in hash2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            if (hash2.Contains(book2))
+            {
+                Console.WriteLine("Этот элемент присутствует в коллекции\n");
+            }
+            else
+            {
+                Console.WriteLine("В коллекции нет такого элемента\n");
+            }
+
+            //4
+            Console.WriteLine("\nЗадание 4\n");
+            ObservableCollection<PrintedEdition> books = new ObservableCollection<PrintedEdition>
+            {
+                new PrintedEdition { Title = "Book11"},
+                new PrintedEdition { Title = "Book12"},
+                new PrintedEdition { Title = "Book13"}
+            };
+            foreach (PrintedEdition i in books)
+            {
+                Console.WriteLine(i.Title);
+            }
+            Console.WriteLine();
+            books.CollectionChanged += Books_CollectionChanged;
+
+            books.Add(new PrintedEdition { Title = "Book14" });
+            books.RemoveAt(1);
+            books[0] = new PrintedEdition { Title = "Book15" };
+
+            foreach (PrintedEdition i in books)
+            {
+                Console.WriteLine(i.Title);
+            }
+
+            Console.ReadKey();
+        }
+        private static void Books_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add: // добавление
+                    PrintedEdition newBook = e.NewItems[0] as PrintedEdition;
+                    Console.WriteLine($"Добавлен новый объект: {newBook.Title}");
+                    break;
+                case NotifyCollectionChangedAction.Remove: // удаление
+                    PrintedEdition oldBook = e.OldItems[0] as PrintedEdition;
+                    Console.WriteLine($"Удален объект: {oldBook.Title}");
+                    break;
+                case NotifyCollectionChangedAction.Replace: // замена
+                    PrintedEdition replacedBook = e.OldItems[0] as PrintedEdition;
+                    PrintedEdition replacingBook = e.NewItems[0] as PrintedEdition;
+                    Console.WriteLine($"Объект {replacedBook.Title} заменен объектом {replacingBook.Title}");
+                    break;
             }
         }
     }
