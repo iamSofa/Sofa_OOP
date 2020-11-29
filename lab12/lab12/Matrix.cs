ebe3e628c3ed7bfe5684bf88abd86ccb7fb591eb
iamSofa <kuskova-sofya@mail.ru>
2020-11-29 13:06:30 +0300

    Добавлена 12 лаба 2 задание

---
+++ lab12/lab12/Matrix.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    class Matrix
+    {
+        public int length;
+        public int width;
+        public int[,] matrix;
+        public Matrix(int a, int b)
+        {
+            matrix = new int[a, b];
+            length = a;
+            width = b;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+        public void AddRandom()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    matrix[i, j] = rnd.Next(0, 2);
+                }
+            }
+        }
+        public void Output()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public int NumberOfUnits(int a)
+        {
+            int num = 0;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (matrix[i, j] == a)
+                    {
+                        num++;
+                    }
+                }
+            }
+            return num;
+        }
+        public int SumMartix()
+        {
+            int num = 0;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    num += matrix[i, j];
+                }
+            }
+            return num;
+        }
+    }
+}
---
+++ lab12/lab12/Reflector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    class Reflector
+    {
+        public static void  GetMethods(string objName)
+        {
+            Type myType = Type.GetType(objName);
+
+            Console.WriteLine("Методы:\n");
+            foreach (MethodInfo method in myType.GetMethods())
+            {
+                Console.Write($" {method.ReturnType.Name} {method.Name} (");
+                ParameterInfo[] parameters = method.GetParameters();
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    Console.Write($"{parameters[i].ParameterType.Name} {parameters[i].Name}");
+                    if (i + 1 < parameters.Length) Console.Write(", ");
+                }
+                Console.WriteLine(")");
+            }
+        }
+    }
+}
